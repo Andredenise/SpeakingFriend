07c1135f193de29b83c9b3d58298b98901e5383e
Andre Denise <r0673226@student.luca-arts.be>
2022-01-07 16:53:08 +0100

    Add files via upload

---
+++ Speaking friend/voice_cloning_files/encoder_voice_cloning/data_objects/__init__.py
@@ -0,0 +1,2 @@
+from encoder_voice_cloning.data_objects.speaker_verification_dataset import SpeakerVerificationDataset
+from encoder_voice_cloning.data_objects.speaker_verification_dataset import SpeakerVerificationDataLoader
