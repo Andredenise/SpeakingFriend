4434d3b312ff8fca4c8329f90190853dc846851d
Andre Denise <r0673226@student.luca-arts.be>
2022-01-07 16:58:19 +0100

    Delete Speaking friend/voice_cloning_files/encoder_voice_cloning/data_objects directory

--- Speaking friend/voice_cloning_files/encoder_voice_cloning/data_objects/__init__.py
+++
@@ -1,2 +0,0 @@
-from encoder_voice_cloning.data_objects.speaker_verification_dataset import SpeakerVerificationDataset
-from encoder_voice_cloning.data_objects.speaker_verification_dataset import SpeakerVerificationDataLoader
