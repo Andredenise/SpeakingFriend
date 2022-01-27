eb283b84d912bb267417d4a71db139d82d26d98c
AlexSteveChungAlvarez <schunga@uni.pe>
2022-01-27 18:57:17 -0500

    Version 1.0

---
+++ Blender_Bot.py
@@ -0,0 +1,16 @@
+from transformers import BlenderbotTokenizer, BlenderbotForConditionalGeneration # Import the model class and the tokenizer for the model
+class BB:
+    def __init__(self):
+        self.tokenizer = None
+        self.model = None
+        self._load()
+
+    def _load(self):
+        self.tokenizer = BlenderbotTokenizer.from_pretrained("facebook/blenderbot-400M-distill") # Download and setup the model and tokenizer
+        self.model = BlenderbotForConditionalGeneration.from_pretrained("facebook/blenderbot-400M-distill")
+
+    def get_answer(self,voice_data):
+        inputs = self.tokenizer(voice_data, return_tensors="pt") # Tokenize the utterance
+        res = self.model.generate(**inputs) # Passing through the utterances to the Blenderbot model
+        output = self.tokenizer.decode(res[0]) # Decoding the model output
+        return output
--- Main_Program.py
+++ Main_Program.py
@@ -1,40 +1,9 @@
-import os # to remove creat
-import tensorflow as tf
-from voice_cloning_files.synthesizer.utils import text
-from voice_cloning_files.synthesizer.inference import Synthesizer
-import voice_cloning_files.encoder_voice_cloning.inference as encoder
-import voice_cloning_files.vocoder_voice_cloning.inference as vocoder
-from pathlib import Path
-import os.path
-from tqdm import trange
-from time import sleep, time
-from transformers import BlenderbotTokenizer, BlenderbotForConditionalGeneration # Import the model class and the tokenizer for the model
-
-# First we have to make a chat.txt directory where we send all input and outpu text to so we have a document with the whole conversation
-# Function to check if chat.txt already exist
-file_name = r"directory of the chat txt file"
-if os.path.isfile(file_name):
-    expand = 1
-    while True:
-        expand += 1
-        new_file_name = file_name.split(".txt")[0] + str(expand) + ".txt"
-        if os.path.isfile(new_file_name):
-            continue
-        else:
-            file_name = new_file_name
-            break
-chat = open(file_name, 'w')  
-
+from Voice_Cloning import RTVC
 
 # Load the models and create the speaker embedding.
 # This only needs to be done once, at the beginning of your script.
 # We have to do this in the begin before the while loop because it only has to be done once
-print("Preparing the encoder, the synthesizer and the vocoder...")
-encoder.load_model(Path("voice_cloning_files/encoder_voice_cloning/saved_models/pretrained.pt"))
-synthesizer = Synthesizer(Path("voice_cloning_files/synthesizer/saved_models/pretrained/pretrained.pt"))
-vocoder.load_model(Path("voice_cloning_files/vocoder_voice_cloning/saved_models/pretrained/pretrained.pt"))
-
-
+rtvc = RTVC()
 
 # Printed text to inform the speaker what to do and how the program works 
 # How to quit, How to record,...
@@ -42,10 +11,12 @@ vocoder.load_model(Path("voice_cloning_files/vocoder_voice_cloning/saved_models/
 
 # Here has to start a while loop untill speaker wants to stop the conversation
 # -----------------------------------------------
-
+while 1:
 # Function to call Record_Speaker and record the speaker
 # -----------------------------------------------
-
+    voice_data = rtvc.answer()
+    if "bye" in voice_data:
+        break
 # Function to get output from Record_Speaker to Interact_gpt_model
 # -----------------------------------------------
 
@@ -55,16 +26,6 @@ vocoder.load_model(Path("voice_cloning_files/vocoder_voice_cloning/saved_models/
 # Alternative for the model, instant call function for other model
 # -----------------------------------------------
 
-tokenizer = BlenderbotTokenizer.from_pretrained("facebook/blenderbot-400M-distill") # Download and setup the model and tokenizer
-model = BlenderbotForConditionalGeneration.from_pretrained("facebook/blenderbot-400M-distill")
-
-utterance = "Here has to come the output of the record_speaker --> voice_data"
-
-inputs = tokenizer(utterance, return_tensors="pt") # Tokenize the utterance
-
-res = model.generate(**inputs) # Passing through the utterances to the Blenderbot model
-
-output = tokenizer.decode(res[0]) # Decoding the model output
 
 # Function to start Voice_cloning.py
 # -----------------------------------------------
